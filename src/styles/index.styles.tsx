f011be09386e618d4c3e738451151a11e5de1c36
Carlos && Ponce <srtager555@gmail.com>
2023-06-18 09:56:27 -0600

    first styled component created: Background

---
+++ src/styles/index.styles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: conic-gradient(
+    from 0deg at 50% 130.83%,
+    #f8f8f8 0deg,
+    #d0d0d0 28.98deg,
+    #cccccc 180deg,
+    #d0d0d0 333.13deg,
+    #f8f8f8 360deg
+  );
+  z-index: -1;
+`;
