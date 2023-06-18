58c123ab3a2ee577cb5aa2ec2f5503a9352d26ca
Carlos && Ponce <srtager555@gmail.com>
2023-06-18 09:59:54 -0600

    Main Layout added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Main__layout } from "@/layouts/Main.layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Main__layout>
+      <Component {...pageProps} />
+    </Main__layout>
+  );
 }
