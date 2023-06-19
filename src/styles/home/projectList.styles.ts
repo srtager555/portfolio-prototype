b366b023b07770c09d64f0b2897c64b04a2c9d2a
Carlos && Ponce <srtager555@gmail.com>
2023-06-18 19:32:53 -0600

    first styles to the project list ready

---
+++ src/styles/home/projectList.styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const FlexContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ProjectCard = styled.div`
+  margin: 0 auto;
+  width: 15vw;
+  overflow: hidden;
+`;
+
+export const TranslateContainer = styled.div<{ loaded: boolean }>`
+  transform: ${({ loaded }) => (loaded ? "translateY(0%)" : "translateY(100%)")};
+  transition: ${({ loaded }) => (loaded ? "700ms ease-in-out" : "")};
+`;
+
+export const ImageContainer = styled.div`
+  & img {
+    width: 100%;
+  }
+`;
+
+export const ProjectName = styled.p``;
