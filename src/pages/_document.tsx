ce672dc4ddd3420499494258d8d76e099e828e80
Carlos && Ponce <srtager555@gmail.com>
2023-06-19 22:51:06 -0600

    Montserrat changed to League Gothic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,700;1,200;1,400;1,600&family=Noto+Sans:ital,wght@0,400;0,500;1,400;1,500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=League+Gothic&family=Noto+Sans:ital,wght@0,400;0,500;1,400;1,500&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
       <body>
         <Main />
