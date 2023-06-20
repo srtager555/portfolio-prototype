7bbdd3395055a5d92220f87033ee7057cff9b326
Carlos && Ponce <srtager555@gmail.com>
2023-06-19 22:51:31 -0600

    Animation when the component is loaded updated

--- src/styles/home/index.styles.ts
+++ src/styles/home/index.styles.ts
@@ -2,20 +2,24 @@ import styled from "styled-components";
 
 export const BGTitle = styled.div<{ loaded: boolean }>`
   position: fixed;
-  top: 50%;
+  top: ${({ loaded }) => (loaded ? "25%" : "50%")};
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: -1;
-  font-size: 15rem;
+  font-size: 25rem;
   white-space: nowrap;
   text-transform: uppercase;
   font-weight: 700;
   overflow: hidden;
+  color: #fff;
+  mix-blend-mode: exclusion;
+  pointer-events: none;
+  transition: top 500ms ease-in-out;
 
   & span {
-    font-family: "Montserrat";
+    font-family: "League Gothic";
     display: inline-block;
-    transition: transform 250ms ease-in-out, top 250ms ease-in-out;
+    transition: transform 500ms ease-in-out, top 500ms ease-in-out;
   }
 
   & span:nth-child(1) {
