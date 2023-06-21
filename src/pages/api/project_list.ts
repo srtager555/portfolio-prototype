d746fd25e6165a3d1cba7258c2184d35771fa4b9
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 14:04:45 -0600

    API updated

--- src/pages/api/project_list.ts
+++ src/pages/api/project_list.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { GetProjectFace } from "@/ddbb/projects";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  req.statusCode = 200;
+  const projects = GetProjectFace();
+
+  res.status(200).json(projects);
 }
