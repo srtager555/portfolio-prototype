9054ba2656df1835208b0e21d58459c7753bc51b
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 15:07:14 -0600

    new component created: Filter

--- src/styles/index.styles.tsx
+++ src/styles/index.styles.tsx
@@ -9,3 +9,16 @@ export const Background = styled.div`
   background: conic-gradient(at 50% 130.83%, #f8f8f8, #a1a1a1 15%, #595959, #a1a1a1 85%, #f8f8f8);
   z-index: -10;
 `;
+
+export const Filter = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-image: url("https://i.ibb.co/WgB2W5y/filter.jpg");
+  background-repeat: repeat;
+  z-index: 999;
+  pointer-events: none;
+  opacity: 0.02;
+`;
