b6ab82cde1711f3cd669409a0d8ad72bbe5ae420
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 09:12:44 -0600

    Adding the responsive design to the mobile version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { BGTitle } from "@/styles/home/index.styles";
+import { isMobile } from "react-device-detect";
+import { BGTitleDesktop, BGTitleMobile, TextAnimation } from "@/styles/home/index.styles";
 import { useEffect, useState } from "react";
 import { ProjectList } from "@/components /ProjectList";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
@@ -17,10 +12,17 @@ export default function Home() {
 
   return (
     <>
-      <BGTitle loaded={loaded}>
-        <span>CARLOS PONCE</span>
-        <span>PORTFOLIO</span>
-      </BGTitle>
+      {isMobile ? (
+        <BGTitleDesktop loaded={loaded}>
+          <span>CARLOS PONCE</span>
+          <span>PORTFOLIO</span>
+        </BGTitleDesktop>
+      ) : (
+        <BGTitleMobile>
+          <TextAnimation loaded={loaded}>Carlos</TextAnimation>
+          <TextAnimation loaded={loaded}>Ponce</TextAnimation>
+        </BGTitleMobile>
+      )}
       <ProjectList />
     </>
   );
