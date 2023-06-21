35f4760d5a1bb996d4503f668570a037c288fe52
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 14:04:26 -0600

    get all project function added

---
+++ src/ddbb/projects/index.ts
@@ -0,0 +1,7 @@
+import barbershop_app from "./barbershop_app";
+import wrapping_letters from "./wrapping_letters";
+import this_page from "./this_page";
+
+export function GetProjectFace() {
+  return [wrapping_letters, this_page, barbershop_app];
+}
