a71c19fab624e35238af7299d25de840e2b8a038
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 14:04:36 -0600

    project_face changed

--- src/ddbb/projects/barbershop_app.ts
+++ src/ddbb/projects/barbershop_app.ts
@@ -1,7 +1,7 @@
 const barbershop_app: CardData = {
   id: "barbershop_app",
   name: "Barbershop app",
-  project_face: "https://i.ibb.co/GQsrPwx/imagen-22222.jpg",
+  project_face: "https://i.ibb.co/k95B6Mm/Ahri-de-perfil.jpg",
   project_url: "/barbershop_app",
 };
 
