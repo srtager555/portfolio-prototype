16dda5ba03a5ea13108415b4dc985a0d95da2eef
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 13:42:49 -0600

    wrapping letters main data added

---
+++ src/ddbb/projects/wrapping_letters.ts
@@ -0,0 +1,8 @@
+const wrapping_letters: CardData = {
+  name: "Wrapping Letters",
+  project_url: "/wrapping_letters",
+  project_face:
+    "https://i.ibb.co/K7KxrMp/snow-moon-ahri-skin-lol-splash-art-phone-wallpaper-hd-uhdpaper-com-593-1-k.jpg",
+};
+
+export default wrapping_letters;
