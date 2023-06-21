0a80453ca84769f1b4aa1245b593a1de24f3a87f
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 09:24:11 -0600

    Issue about hydratation error fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,32 @@ import { ProjectList } from "@/components /ProjectList";
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
+  const [mobile, setMobile] = useState(true);
 
   useEffect(() => {
     setTimeout(() => setLoaded(true), 1500);
   }, []);
 
+  useEffect(() => {
+    if (!isMobile) setMobile(false);
+    else setMobile(true);
+  }, []);
+
   return (
     <>
-      {isMobile ? (
+      {!mobile ? (
         <BGTitleDesktop loaded={loaded}>
           <span>CARLOS PONCE</span>
           <span>PORTFOLIO</span>
         </BGTitleDesktop>
       ) : (
         <BGTitleMobile>
-          <TextAnimation loaded={loaded}>Carlos</TextAnimation>
-          <TextAnimation loaded={loaded}>Ponce</TextAnimation>
+          <TextAnimation loaded={loaded}>
+            <span>Carlos</span>
+          </TextAnimation>
+          <TextAnimation loaded={loaded}>
+            <span>Ponce</span>
+          </TextAnimation>
         </BGTitleMobile>
       )}
       <ProjectList />
