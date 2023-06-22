30e4ffc8dcb990ba2aa18582e648dc540b1b223a
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 18:02:28 -0600

    issue in the resize event listener fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { BGTitleDesktop, BGTitleMobile, TextAnimation } from "@/styles/home/inde
 import { useEffect, useState } from "react";
 import { ProjectList } from "@/components /ProjectList";
 import { GetStaticProps, NextPage } from "next";
+import { Opacity } from "@/styles/index.styles";
 
 // gsp removed for a issue time
 // export const getStaticProps: GetStaticProps = async (ctx) => {
@@ -18,6 +19,8 @@ import { GetStaticProps, NextPage } from "next";
 // };
 
 const Home: NextPage = function () {
+  const [firstPaint, setFirstPaint] = useState(true);
+  const [opacityLoaded, setOpacityLoaded] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const [mobile, setMobile] = useState(true);
   const [cardData, setCardData] = useState<CardData[]>([
@@ -31,10 +34,6 @@ const Home: NextPage = function () {
   ]);
 
   useEffect(() => {
-    setTimeout(() => setLoaded(true), 1500);
-  }, []);
-
-  useEffect(() => {
     async function fetchProjects() {
       const projects: CardData[] = await fetch("/api/project_list").then((data) => data.json());
 
@@ -45,6 +44,17 @@ const Home: NextPage = function () {
 
   useEffect(() => {
     function Resize() {
+      if (firstPaint)
+        setTimeout(() => {
+          if (window.innerWidth >= 1024) setMobile(false);
+          else setMobile(true);
+
+          setFirstPaint(false);
+          setOpacityLoaded(true);
+
+          setTimeout(() => setLoaded(true), 1000);
+        }, 1000);
+
       if (window.innerWidth >= 1024) setMobile(false);
       else setMobile(true);
     }
@@ -59,7 +69,7 @@ const Home: NextPage = function () {
   }, []);
 
   return (
-    <>
+    <Opacity loaded={opacityLoaded}>
       {!mobile ? (
         <BGTitleDesktop loaded={loaded}>
           <span>CARLOS PONCE</span>
@@ -78,8 +88,8 @@ const Home: NextPage = function () {
           </TextAnimation>
         </BGTitleMobile>
       )}
-      <ProjectList CardData={cardData} />
-    </>
+      <ProjectList CardData={cardData} loaded={loaded} />
+    </Opacity>
   );
 };
 
