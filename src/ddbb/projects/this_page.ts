61e2bae611312837120c40a8cac30af35c0d2c19
Carlos && Ponce <srtager555@gmail.com>
2023-06-21 21:13:47 -0600

    image changed to fix the issue about the height

--- src/ddbb/projects/this_page.ts
+++ src/ddbb/projects/this_page.ts
@@ -1,7 +1,8 @@
 const this_page: CardData = {
   id: "this_page",
   name: "This Page",
-  project_face: "https://i.ibb.co/b56T388/Orianna-full-body.jpg",
+  project_face:
+    "https://i.ibb.co/d7rgnym/challenger-ahri-asu-lol-skin-splash-art-phone-wallpaper-hd-uhdpaper-com-649-1-j.jpg",
   project_url: "/this_page",
 };
 
