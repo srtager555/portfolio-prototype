ba9fb83d5dbb21da96f6781b606599e341f51a2b
Carlos && Ponce <srtager555@gmail.com>
2023-06-23 20:13:42 -0600

    syntax to get the project data ready

--- src/pages/[id_project].tsx
+++ src/pages/[id_project].tsx
@@ -1,3 +1,31 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 export default function Project() {
+  const { id_project } = useRouter().query;
+  const [data, setData] = useState<ProjectData>();
+  const [notFound, setNotFound] = useState<ProjectData>();
+
+  useEffect(() => {
+    async function getProject() {
+      if (!id_project) return;
+
+      const project = await fetch("/api/getProject/", {
+        method: "POST",
+        body: JSON.stringify({
+          id_project,
+        }),
+      }).then((data) => data.json());
+
+      if (!project) return;
+
+      setData(project);
+    }
+  }, [id_project]);
+
+  if (!data) <>loading</>;
+
+  if (notFound) <>404</>;
+
   return <>a</>;
 }
