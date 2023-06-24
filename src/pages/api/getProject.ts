917b03f6b020ea3f4c122f4f8d7203de0c8d061f
Carlos && Ponce <srtager555@gmail.com>
2023-06-23 20:43:10 -0600

    first api prototype logic ready

--- src/pages/api/getProject.ts
+++ src/pages/api/getProject.ts
@@ -1,6 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { findProject } from "@/ddbb/projects/project_data/index";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const body: { id_project: string } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+  const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
   const { id_project } = body;
+
+  // the atteibute "id_project" must be a strig
+  if (typeof id_project != "string") {
+    res.status(406).json("the api require a id that must be a string");
+  } else {
+    const project = findProject({ id: id_project as any });
+
+    if (!project) {
+      res.status(404);
+    } else {
+      res.status(200).json(project);
+    }
+  }
 }
