0513ec002e64cc1a1f6d0c25e6ac76f4cdfcb129
Carlos && Ponce <srtager555@gmail.com>
2023-06-23 18:54:31 -0600

    Hel with default settings added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import { Helmet } from "@/layouts/Helmet.layout";
 import { Main__layout } from "@/layouts/Main.layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Main__layout>
-      <Component {...pageProps} />
-    </Main__layout>
+    <Helmet>
+      <Main__layout>
+        <Component {...pageProps} />
+      </Main__layout>
+    </Helmet>
   );
 }
