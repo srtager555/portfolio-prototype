b12c0e0c775a2211dcf0ca29b5934d6c8c1328f9
Carlos && Ponce <srtager555@gmail.com>
2023-06-23 20:07:52 -0600

    data to wrapping letters added

---
+++ src/ddbb/projects/project_data/wrapping_letters.ts
@@ -0,0 +1,18 @@
+const wrapping_letters: ProjectData = {
+  image: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+  title: "Wrapping Letters",
+  desc: "Make more with less in your text animation with Wrapping Letters",
+  content: `
+    Wrapping Letters is a powerful package designed specifically for React, 
+    soon to be available in JavaScript as well. It aims to streamline working with letters, 
+    making it remarkably easy to wrap them within various JSX elements. With its simple syntax, 
+    Wrapping Letters not only enables you to create visually appealing text animations but also allows you to work with events effortlessly. 
+    You can execute any function related to a specific word without the hassle of searching for it in a lengthy paragraph. This package simplifies the process and enhances your ability to work with letters in a dynamic and engaging way.
+  `,
+  galery: [
+    "https://i.ibb.co/SsX859q/portfolio-image.png",
+    "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+    "https://i.ibb.co/SfNgq36/Fl4bpg-Xk-AI3-Sti.jpg",
+    "https://i.ibb.co/yPzfbjX/FYu-CVc-UVUAA94lm.jpg",
+  ],
+};
