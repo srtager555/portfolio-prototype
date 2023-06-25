33b02e466f870f315a40334296e48ef70b27c274
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 17:39:54 -0600

    I added responsive styles to the title

--- src/styles/projects/Header.styles.tsx
+++ src/styles/projects/Header.styles.tsx
@@ -17,4 +17,16 @@ export const Title = styled.h1`
   font-size: 10rem;
   font-family: "League Gothic";
   text-align: center;
+
+  @media (max-width: 1024px) {
+    font-size: 6rem;
+  }
+
+  @media (max-width: 414px) {
+    font-size: 4rem;
+  }
+
+  @media (max-width: 375px) {
+    font-size: 2rem;
+  }
 `;
