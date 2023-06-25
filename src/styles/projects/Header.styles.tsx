bdf8f9da15cabef4990d386d39a87af97dce3a5d
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 16:09:38 -0600

    I started to created the styled component to the header

---
+++ src/styles/projects/Header.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  margin-bottom: 150px;
+`;
+
+export const Title = styled.h1`
+  font-size: 10rem;
+  font-family: "League Gothic";
+`;
