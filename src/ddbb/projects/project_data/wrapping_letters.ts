df356ca173528c01dbc4a1c698424fc36f066683
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 20:37:44 -0600

    typing updated

--- src/ddbb/projects/project_data/wrapping_letters.ts
+++ src/ddbb/projects/project_data/wrapping_letters.ts
@@ -1,5 +1,8 @@
 const wrapping_letters: ProjectData = {
-  image: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+  image: {
+    image: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+    imageBlur: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+  },
   title: "Wrapping Letters",
   desc: "Make more with less in your text animation with Wrapping Letters",
   content: `
@@ -8,10 +11,22 @@ const wrapping_letters: ProjectData = {
     Say goodbye to searching in long paragraphs. This package enhances your ability to work dynamically and engage with letters.
   `,
   galery: [
-    "https://i.ibb.co/SsX859q/portfolio-image.png",
-    "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
-    "https://i.ibb.co/SfNgq36/Fl4bpg-Xk-AI3-Sti.jpg",
-    "https://i.ibb.co/yPzfbjX/FYu-CVc-UVUAA94lm.jpg",
+    {
+      image: "https://i.ibb.co/SsX859q/portfolio-image.png",
+      imageBlur: "https://i.ibb.co/SsX859q/portfolio-image.png",
+    },
+    {
+      image: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+      imageBlur: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+    },
+    {
+      image: "https://i.ibb.co/SfNgq36/Fl4bpg-Xk-AI3-Sti.jpg",
+      imageBlur: "https://i.ibb.co/SfNgq36/Fl4bpg-Xk-AI3-Sti.jpg",
+    },
+    {
+      image: "https://i.ibb.co/yPzfbjX/FYu-CVc-UVUAA94lm.jpg",
+      imageBlur: "https://i.ibb.co/yPzfbjX/FYu-CVc-UVUAA94lm.jpg",
+    },
   ],
 };
 
--- src/ddbb/projects/wrapping_letters.ts
+++ src/ddbb/projects/wrapping_letters.ts
@@ -2,8 +2,11 @@ const wrapping_letters: CardData = {
   id: "wrapping_letters",
   name: "Wrapping Letters",
   project_url: "/wrapping_letters",
-  project_face:
-    "https://i.ibb.co/K7KxrMp/snow-moon-ahri-skin-lol-splash-art-phone-wallpaper-hd-uhdpaper-com-593-1-k.jpg",
+  project_face: {
+    image: "https://i.ibb.co/K7KxrMp/snow-moon-ahri-skin-lol-splash-art-phone-wallpaper-hd-uhdpaper-com-593-1-k.jpg",
+    imageBlur:
+      "https://i.ibb.co/K7KxrMp/snow-moon-ahri-skin-lol-splash-art-phone-wallpaper-hd-uhdpaper-com-593-1-k.jpg",
+  },
 };
 
 export default wrapping_letters;
