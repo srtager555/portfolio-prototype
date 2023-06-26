dacbb1684d5472011163092cef6e722aba62c780
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 18:36:27 -0600

    styles to the Content added

---
+++ src/styles/projects/Content.styles.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Content = styled.p`
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+  margin-bottom: 100px;
+  font-size: 1.7rem;
+  font-weight: 300;
+
+  @media (max-width: 514px) {
+    width: 90%;
+  }
+`;
