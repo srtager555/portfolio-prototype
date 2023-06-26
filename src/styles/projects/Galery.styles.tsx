5e94388ab6405fa33e1ee6db39b2f3d10b2ea0df
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 19:28:29 -0600

    styles to the galery created

---
+++ src/styles/projects/Galery.styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+`;
