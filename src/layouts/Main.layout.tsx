4bdbef14d612032f43eaa6225e4548fd69d7d725
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 20:55:52 -0600

    navbar imported

--- src/layouts/Main.layout.tsx
+++ src/layouts/Main.layout.tsx
@@ -1,8 +1,10 @@
+import { Nav } from "@/components/navbar";
 import { Background, Container, Filter } from "@/styles/index.styles";
 
 export function Main__layout({ children }: { children: children }) {
   return (
     <Container>
+      <Nav />
       <Filter />
       <Background />
       {children}
