e52f5fc80a0861c12ee0e319771fffc302db5632
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 20:25:44 -0600

    Img updated

--- src/styles/projects/Galery.styles.tsx
+++ src/styles/projects/Galery.styles.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import { ImageDefault } from "../index.styles";
 
 export const ImageContainer = styled.div`
   width: 100%;
   margin-bottom: 20px;
 `;
 
-export const Img = styled.img`
+export const Img = styled(ImageDefault)`
   width: 100%;
 `;
