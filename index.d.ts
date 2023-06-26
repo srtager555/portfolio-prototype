92c06c88237e7fa929cd153a8930d1734f05970e
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 20:32:44 -0600

    new type added

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,21 @@
 type children = JSX.Element | JSX.Element[] | string;
 
+type img = {
+  image: string;
+  imageBlur: string;
+};
+
 type CardData = {
   id: string;
   project_url: string;
-  project_face: string;
+  project_face: img;
   name: string;
 };
 
 type ProjectData = {
-  image: string;
+  image: img;
   title: string;
   desc: string;
   content: string;
-  galery: string[];
+  galery: img[];
 };
