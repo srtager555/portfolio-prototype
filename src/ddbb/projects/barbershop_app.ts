52a44826e5c17a3e62ee8d481bc1fb511e513c92
Carlos && Ponce <srtager555@gmail.com>
2023-06-25 20:42:32 -0600

    typing updated

--- src/ddbb/projects/barbershop_app.ts
+++ src/ddbb/projects/barbershop_app.ts
@@ -1,7 +1,10 @@
 const barbershop_app: CardData = {
   id: "barbershop_app",
   name: "Barbershop app",
-  project_face: "https://i.ibb.co/k95B6Mm/Ahri-de-perfil.jpg",
+  project_face: {
+    image: "https://i.ibb.co/k95B6Mm/Ahri-de-perfil.jpg",
+    imageBlur: "https://i.ibb.co/k95B6Mm/Ahri-de-perfil.jpg",
+  },
   project_url: "/barbershop_app",
 };
 
--- src/ddbb/projects/this_page.ts
+++ src/ddbb/projects/this_page.ts
@@ -1,8 +1,12 @@
 const this_page: CardData = {
   id: "this_page",
   name: "This Page",
-  project_face:
-    "https://i.ibb.co/d7rgnym/challenger-ahri-asu-lol-skin-splash-art-phone-wallpaper-hd-uhdpaper-com-649-1-j.jpg",
+  project_face: {
+    image:
+      "https://i.ibb.co/d7rgnym/challenger-ahri-asu-lol-skin-splash-art-phone-wallpaper-hd-uhdpaper-com-649-1-j.jpg",
+    imageBlur:
+      "https://i.ibb.co/d7rgnym/challenger-ahri-asu-lol-skin-splash-art-phone-wallpaper-hd-uhdpaper-com-649-1-j.jpg",
+  },
   project_url: "/this_page",
 };
 
