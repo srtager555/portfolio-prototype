1ac022dbb91ca259f3d5325b8c3cd2fdeafaaa28
Carlos && Ponce <srtager555@gmail.com>
2023-06-26 22:26:27 -0600

    new styles added to the first prototype

--- src/styles/projects/NextProject.styles.tsx
+++ src/styles/projects/NextProject.styles.tsx
@@ -3,23 +3,50 @@ import styled from "styled-components";
 
 export const Container = styled(Link)`
   position: relative;
-  display: block;
+  display: flex;
+  justify-content: space-between;
   width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
   text-decoration: none;
   margin-top: 200px;
   margin-bottom: 100px;
   overflow: hidden;
+  color: #000;
 `;
 
-export const Next = styled.span`
-  display: block;
-  text-align: center;
-  color: #000;
-  /* position: absolute;
-  transform: translate(-50%, -50%);
-  top: -5%;
-  left: 50%; */
+export const DataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media (max-width: 1440px) {
+    padding-left: 10px;
+  }
+`;
+
+export const Name = styled.h2`
   font-size: 10rem;
   font-family: "League Gothic";
-  white-space: nowrap;
+`;
+
+export const Next = styled.p`
+  font-size: 1.5rem;
+  font-weight: 300;
+`;
+
+export const ImageContainer = styled.div`
+  width: 50%;
+`;
+
+export const Go = styled.span`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  border-radius: 100%;
+  border: solid 1px #000;
+  font-size: 5rem;
+  font-family: "League Gothic";
 `;
