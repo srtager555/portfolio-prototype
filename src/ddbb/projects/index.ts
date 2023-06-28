c3b2ae530df9ee5b8ba26d86140a228084a7f73b
Carlos && Ponce <srtager555@gmail.com>
2023-06-28 13:49:45 -0600

    function to get the next project face ready

--- src/ddbb/projects/index.ts
+++ src/ddbb/projects/index.ts
@@ -5,3 +5,13 @@ import this_page from "./this_page";
 export function GetProjectFace() {
   return [wrapping_letters, this_page, barbershop_app];
 }
+
+export function GetNextProjectFace(id: "wrapping_letters" | "this_page" | "barbershop_app") {
+  const projectsList = [barbershop_app, wrapping_letters, this_page];
+
+  // if the can find a project so will return the first project
+  const currentProjectIndex = projectsList.indexOf(projectsList.find((_) => _.id === id) ?? projectsList[0]);
+  const nextProjectIndex = currentProjectIndex + 1 > projectsList.length - 1 ? 0 : currentProjectIndex + 1;
+
+  return projectsList[nextProjectIndex];
+}
