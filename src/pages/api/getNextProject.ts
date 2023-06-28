eebe7f57ae7d8f53373b0fb5d462251d4465a933
Carlos && Ponce <srtager555@gmail.com>
2023-06-28 13:56:19 -0600

    logic to get the next project api ready

--- src/pages/api/getNextProject.ts
+++ src/pages/api/getNextProject.ts
@@ -1,5 +1,14 @@
+import { GetNextProjectFace } from "@/ddbb/projects";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  req.statusCode = 200;
+  if (req.method !== "POST") {
+    res.status(405).json("Only post is available");
+  } else {
+    const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+
+    const project = GetNextProjectFace(body.id);
+
+    res.status(200).json(project);
+  }
 }
