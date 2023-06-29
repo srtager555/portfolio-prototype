bb10d81eb5854756be8a164cbd9585932b0829c4
Carlos && Ponce <srtager555@gmail.com>
2023-06-28 20:14:14 -0600

    example data to this page added

--- src/ddbb/projects/project_data/index.ts
+++ src/ddbb/projects/project_data/index.ts
@@ -1,12 +1,14 @@
 import wrapping_letters from "./wrapping_letters";
+import this_page from "./this_page";
 
 interface props {
-  id: "wrapping_letters";
+  id: "wrapping_letters" | "this_page";
 }
 
 export function findProject({ id }: props) {
   const projects = {
     wrapping_letters,
+    this_page,
   };
 
   if (!projects[id]) return;
---
+++ src/ddbb/projects/project_data/this_page.ts
@@ -0,0 +1,33 @@
+const this_page: ProjectData = {
+  image: {
+    image: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+    imageBlur: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+  },
+  title: "This page",
+  desc: "Make more with less in your text animation with Wrapping Letters",
+  content: `
+    Wrapping Letters simplifies letter manipulation in React, making it easy to wrap them in JSX elements.
+    Create visually appealing text animations and effortlessly work with events.
+    Say goodbye to searching in long paragraphs. This package enhances your ability to work dynamically and engage with letters.
+  `,
+  galery: [
+    {
+      image: "https://i.ibb.co/yPzfbjX/FYu-CVc-UVUAA94lm.jpg",
+      imageBlur: "https://i.ibb.co/yPzfbjX/FYu-CVc-UVUAA94lm.jpg",
+    },
+    {
+      image: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+      imageBlur: "https://i.ibb.co/MSscpjs/snow-moon-ahri.jpg",
+    },
+    {
+      image: "https://i.ibb.co/SfNgq36/Fl4bpg-Xk-AI3-Sti.jpg",
+      imageBlur: "https://i.ibb.co/SfNgq36/Fl4bpg-Xk-AI3-Sti.jpg",
+    },
+    {
+      image: "https://i.ibb.co/SsX859q/portfolio-image.png",
+      imageBlur: "https://i.ibb.co/SsX859q/portfolio-image.png",
+    },
+  ],
+};
+
+export default this_page;
