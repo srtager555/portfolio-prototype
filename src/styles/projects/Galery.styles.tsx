c4018bab25d738c0a4f4789182c2edc2ecddbd9e
Carlos && Ponce <srtager555@gmail.com>
2023-06-28 19:05:35 -0600

    conditional added to show the image

--- src/styles/projects/Galery.styles.tsx
+++ src/styles/projects/Galery.styles.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import { ImageDefault } from "../index.styles";
 
-export const ImageContainer = styled.div`
+export const ImageContainer = styled.div<{ inView?: boolean }>`
   width: 100%;
   margin-bottom: 20px;
+  overflow: hidden;
+
+  & img {
+    transform: translateY(${({ inView }) => (inView ? "0%" : "100%")});
+  }
 `;
 
 export const Img = styled(ImageDefault)`
   width: 100%;
+  transition: transform 1.5s ease-in-out;
 `;
