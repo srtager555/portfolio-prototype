b4eb65f5583c6d136cb57a84b3e0e590779cfe9e
Carlos && Ponce <srtager555@gmail.com>
2023-07-02 17:57:25 -0600

    File name changed and new component created

--- src/styles/home/index.new.styles.tsx
+++ src/styles/home/index.new.styles.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
   padding-top: 50px;
+  overflow: hidden;
 `;
 
 export const CorrectFSizeContainer = styled.div`
@@ -19,6 +22,15 @@ export const Introduction = styled.h1`
 `;
 
 export const Description = styled.p`
-  line-height: 85%;
-  overflow: hidden;
+  line-height: 70%;
 `;
+
+export const CourgetteFont = styled.span`
+  /* line-height: 70%; */
+  font-family: "Courgette" !important;
+  font-size: 11rem;
+`;
+
+export function ScriptFont({ letter }: { letter: string }) {
+  return <CourgetteFont>{letter}</CourgetteFont>;
+}
