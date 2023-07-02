0added8f490db1917da6e3dbb484b2315d230324
Carlos && Ponce <srtager555@gmail.com>
2023-07-02 16:58:52 -0600

    I staring to upgrade the home

---
+++ src/components/home/Content.tsx
@@ -0,0 +1,12 @@
+import WL from "wrapping-letters-react";
+
+export function Content() {
+  return (
+    <div>
+      <h1>Hello!, I&apos;m Carlos && Ponce</h1>
+      <p>
+        <WL text="Front-end dev based in Honduras" />
+      </p>
+    </div>
+  );
+}
