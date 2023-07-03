61bf5cb51dce0f44cde7cf9f14bd74060a32c48a
Carlos && Ponce <srtager555@gmail.com>
2023-07-02 22:22:10 -0600

    structure to WL created

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -1,14 +1,50 @@
 import {
   Container,
   CorrectFSizeContainer,
+  DefaultWrapp,
   Description,
   Introduction,
   ScriptFont,
 } from "@/styles/home/index.new.styles";
+//@ts-ignore
 import WL from "wrapping-letters-react";
 
 interface props {
   loaded: boolean;
+}
+
+//@ts-ignore
+function structure({ letter, specialWrapp, loaded }) {
+  const { hasCustomWrapp, NewWrappStructure, hasCustomProps } = specialWrapp;
+
+  let props = {
+    letter,
+    loaded,
+  };
+
+  let Component = () =>
+    hasCustomWrapp
+      ? typeof NewWrappStructure === "object"
+        ? NewWrappStructure.structureToAdd
+        : NewWrappStructure
+      : letter;
+
+  if (hasCustomProps) {
+    Component = NewWrappStructure.structureToAdd;
+
+    return (
+      <DefaultWrapp loaded={loaded}>
+        <Component {...props} {...NewWrappStructure.props} />
+      </DefaultWrapp>
+    );
+  }
+
+  return (
+    <DefaultWrapp loaded={loaded}>
+      {/* @ts-ignore */}
+      <Component {...props} />
+    </DefaultWrapp>
+  );
 }
 
 export function Content({ loaded }: props) {
@@ -22,10 +58,11 @@ export function Content({ loaded }: props) {
             textOptions={{
               SpecialWrapp: {
                 wordToSearch: ["living", "in"],
-                structureToAdd: ScriptFont as unknown as JSX.Element,
+                structureToAdd: { structureToAdd: ScriptFont as unknown as JSX.Element, props: { loaded } },
               },
               PerWord: true,
             }}
+            structure={{ structure: structure, props: { loaded } }}
           />
         </Description>
       </CorrectFSizeContainer>
