1d485ba54b5742c9a619324a9f695405d9a003dd
Carlos && Ponce <srtager555@gmail.com>
2023-07-05 19:41:25 -0600

    sizes changed

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,13 +5,19 @@ const NavBtn = styled.button`
   top: 20px;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 25px;
-  height: 25px;
+  width: 20px;
+  height: 20px;
   background-color: #fff;
   border-radius: 100%;
   border: none;
+  transition: 200ms ease-in-out;
   mix-blend-mode: difference;
   cursor: pointer;
+
+  &:hover {
+    width: 22px;
+    height: 22px;
+  }
 `;
 
 export function Nav() {
