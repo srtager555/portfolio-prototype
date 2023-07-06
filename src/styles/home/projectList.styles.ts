eb610fbbba9098e9a73744d27294705409e47a60
Carlos && Ponce <srtager555@gmail.com>
2023-07-05 19:44:09 -0600

    changing styles to the new design

--- src/styles/home/projectList.styles.ts
+++ src/styles/home/projectList.styles.ts
@@ -2,37 +2,49 @@ import styled from "styled-components";
 import Link from "next/link";
 
 export const FlexContainer = styled.div`
-  position: fixed;
-  top: 60%;
+  /* position: fixed;
+  top: 55%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
   width: 100%;
-  max-width: 1920px;
-  padding: 20px;
-  overflow: scroll;
+  max-width: 1420px;
 
   @media (max-width: 900px) {
     width: 80%;
     padding: 0;
     border-radius: 10px;
+    overflow: scroll;
   }
 `;
 
 export const ProjectCard = styled(Link)`
-  margin: 0 auto;
-  min-width: 225px;
-  max-width: 350px;
-  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  width: 70px;
+  /* min-width: 225px; */
+  max-width: 300px;
+  margin: 0 10px;
+  /* border-radius: 10px; */
   overflow: hidden;
   transition: 200ms ease;
   color: #000;
   text-decoration: none;
+  overflow: hidden;
+
+  & p {
+    opacity: 0;
+  }
 
   &:hover {
+    width: 300px;
     filter: drop-shadow(0px 0px 15px #0005);
+
+    & p {
+      opacity: 1;
+    }
   }
 
   &:active {
@@ -40,7 +52,7 @@ export const ProjectCard = styled(Link)`
   }
 
   @media (max-width: 1280px) {
-    max-width: 325px;
+    max-width: 275px;
   }
 
   @media (min-width: 900px) and (max-width: 1124px) {
@@ -61,22 +73,34 @@ export const ProjectCard = styled(Link)`
 `;
 
 export const TranslateContainer = styled.div<{ loaded: boolean }>`
-  transform: ${({ loaded }) => (loaded ? "translateY(0%)" : "translateY(100%)")};
+  transform: ${
+    {
+      /* ({ loaded }) => (loaded ? "translateY(0%)" : "translateY(100%)") */
+    }
+  };
   transition: ${({ loaded }) => (loaded ? "700ms ease-in-out" : "")};
 `;
 
 export const ImageContainer = styled.div`
+  margin-bottom: -5px;
+  width: 300px;
   & img {
     width: 100%;
   }
 `;
 
 export const ProjectName = styled.p`
+  position: fixed;
+  bottom: 5%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   text-align: center;
-  font-size: 1.5rem;
+  font-size: 3rem;
+  font-weight: 300;
   padding: 10px;
-  background-color: #fff;
   margin-top: -5px;
+  transition: opacity 200ms;
+  pointer-events: none;
 
   @media (max-width: 414px) {
     font-size: 1.2rem;
