495f84731a6cad87c3f363037571103d5bd39809
Carlos && Ponce <srtager555@gmail.com>
2023-07-05 19:43:23 -0600

    new styles to the home created

---
+++ src/styles/home/index.newNew.styles.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  padding: 3%;
+`;
+
+export const Content = styled.div`
+  position: fixed;
+  top: 15%;
+  left: 2%;
+  transform: translateY(-50%);
+  color: #fff;
+  mix-blend-mode: difference;
+  z-index: -1;
+`;
+
+export const Name = styled.h1`
+  font-family: "league Gothic";
+  font-size: 10rem;
+  text-transform: uppercase;
+  letter-spacing: -5px;
+  line-height: 90%;
+`;
+
+export const Rol = styled.h1`
+  font-family: "league Gothic";
+  font-size: 5rem;
+  text-transform: uppercase;
+  letter-spacing: -3px;
+`;
+
+export const Footer = styled.div`
+  position: fixed;
+  bottom: 0%;
+  right: 2%;
+  color: #fff;
+  mix-blend-mode: difference;
+  z-index: -1;
+  text-align: end;
+`;
+
+export const Portfolio = styled.p`
+  font-family: "League Gothic";
+  font-size: 5rem;
+`;
