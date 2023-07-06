c061237627785ad01c2ee3e49e608e9b3d27bda2
Carlos && Ponce <srtager555@gmail.com>
2023-07-05 19:42:00 -0600

    project list reduced and simplificed

--- src/ddbb/projects/index.ts
+++ src/ddbb/projects/index.ts
@@ -2,12 +2,14 @@ import barbershop_app from "./barbershop_app";
 import wrapping_letters from "./wrapping_letters";
 import this_page from "./this_page";
 
+const projects = [wrapping_letters, this_page, barbershop_app];
+
 export function GetProjectFace() {
-  return [wrapping_letters, this_page, barbershop_app];
+  return projects;
 }
 
 export function GetNextProjectFace(id: "wrapping_letters" | "this_page" | "barbershop_app") {
-  const projectsList = [barbershop_app, wrapping_letters, this_page];
+  const projectsList = projects;
 
   // if the can find a project so will return the first project
   const currentProjectIndex = projectsList.indexOf(projectsList.find((_) => _.id === id) ?? projectsList[0]);
