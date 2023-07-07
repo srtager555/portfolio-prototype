d02eacb32ab445d28e9658a33089d42727f4a0c5
Carlos && Ponce <srtager555@gmail.com>
2023-07-07 17:08:10 -0600

    Loader imported and Context creatad to use it

--- src/layouts/Main.layout.tsx
+++ src/layouts/Main.layout.tsx
@@ -1,13 +1,22 @@
+import { Loader } from "@/components/Loader.interface";
 import { Nav } from "@/components/navbar";
 import { Background, Container, Filter } from "@/styles/index.styles";
+import { Dispatch, SetStateAction, createContext, useState } from "react";
+
+export const Context = createContext<{ setLoaded: Dispatch<SetStateAction<boolean>> } | null>(null);
 
 export function Main__layout({ children }: { children: children }) {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <Container>
-      {/* <Nav /> */}
-      <Filter />
-      <Background />
-      {children}
-    </Container>
+    <Context.Provider value={{ setLoaded }}>
+      <Container>
+        {/* <Nav /> */}
+        <Filter />
+        <Background />
+        <Loader loaded={loaded} />
+        {children}
+      </Container>
+    </Context.Provider>
   );
 }
